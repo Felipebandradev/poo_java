5dab2d36095eaea1f6fa4455fa39be61d1e81a3d
Felipe Barbosa Brito de Andrade <129704261+Felipebandradev@users.noreply.github.com>
2025-03-11 22:09:31 -0300

    New: add classe_carro
    POO com Carro

---
+++ poo/src/Carro.java
@@ -0,0 +1,23 @@
+// No arquivo Carro.java
+public class Carro {
+    // Propriedades (Atributos) - Caracteristicas
+    int quantidadePortas;
+    String cor;
+    Modelo modelo;
+
+    // Métodos - Ações
+    public void acelerar() {
+        // Código que fará o carro acelerar
+    }
+
+    public void frear() {
+        //Código que fará o carro frear
+    }
+}
+
+enum Modelo{
+    HATCH,
+    SEDAN,
+    SUV,
+    UTILITARIO
+}
---
+++ poo/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Criando o objeto Carro
+        Carro carroA = new Carro();
+
+        // Defindo os atributos do carroA
+        carroA.quantidadePortas = 4;
+        carroA.cor = "Azul";
+        carroA.modelo = Modelo.HATCH;
+
+
+    }
+}
